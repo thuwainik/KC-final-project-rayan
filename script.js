3ae394778e12f60097d2120dff3c24f48874096f
thuwainik <thuwainik@gmail.com>
2023-09-13 04:57:31 +0300

    home

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function increase(name, score) {
 var navbar = `
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">صلاتي</a>
+      <a class="navbar-brand" href="./index.html">صلاتي</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
